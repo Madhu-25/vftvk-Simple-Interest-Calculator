61b2be7b9f53f80c0a4b282456b2c6141d6777c5
Madhu-25 <65850954+Madhu-25@users.noreply.github.com>
2021-01-25 09:28:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+  var principal = document.getElementById("principal").value;
+
+  var rate = document.getElementById("rate").value;
+  var yrs = document.getElementById("years").value;
+  var si = (principal*rate*yrs)/100;
+  output=document.getElementById("result");
+  var yyyy = 2021+Number(yrs);
+  output.innerHTML="<br><br>"+"If you deposit <mark>"+principal+"</mark><br>"+"at an interest rate of <mark>"+rate+"</mark><br>"+"You will receive an amount of <mark>"+si+"</mark><br>"+"in the year <mark>"+yyyy+"</mark>"
+  
     
 }
-        
+
+function slide()
+{
+  var rate = document.getElementById("rate").value;
+  document.getElementById("rate1").innerHTML=rate;
+}
