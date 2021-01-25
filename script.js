56fce1e9d4748a63b73a707f191b87fee8cfffc8
Madhu-25 <65850954+Madhu-25@users.noreply.github.com>
2021-01-25 11:40:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function compute()
   var si = (amt*rate*yrs)/100;
   output=document.getElementById("result");
   var yyyy = 2021+Number(yrs);
-  output.innerHTML="<br><br>"+"If you deposit <mark>"+amt+"</mark><br>"+"at an interest rate of <mark>"+rate+"</mark><br>"+"You will receive an amount of <mark>"+si+"</mark><br>"+"in the year <mark>"+yyyy+"</mark>"
+  output.innerHTML="<br><br>"+"If you deposit <mark>"+amt+"</mark><br>"+"at an interest rate of <mark>"+rate+"%</mark><br>"+"You will receive an amount of <mark>"+si+"</mark><br>"+"in the year <mark>"+yyyy+"</mark>"
   
   
 }
