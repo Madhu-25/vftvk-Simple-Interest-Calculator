f930144625ada7bd50526d665404d31fae9dd680
Madhu-25 <65850954+Madhu-25@users.noreply.github.com>
2021-01-25 11:34:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function compute()
 {
-  var principal = document.getElementById("principal").value;
-  if(principal<=0)
+  var amt = document.getElementById("principal").value;
+  var principal= document.getElementById("principal");
+  if(amt<=0)
   {
     alert("Enter positive number..")
     principal.focus();
@@ -10,12 +11,12 @@ function compute()
 
   var rate = document.getElementById("rate").value;
   var yrs = document.getElementById("years").value;
-  var si = (principal*rate*yrs)/100;
+  var si = (amt*rate*yrs)/100;
   output=document.getElementById("result");
   var yyyy = 2021+Number(yrs);
   output.innerHTML="<br><br>"+"If you deposit <mark>"+principal+"</mark><br>"+"at an interest rate of <mark>"+rate+"</mark><br>"+"You will receive an amount of <mark>"+si+"</mark><br>"+"in the year <mark>"+yyyy+"</mark>"
   
-  return true
+  
 }
 
 function slide()
